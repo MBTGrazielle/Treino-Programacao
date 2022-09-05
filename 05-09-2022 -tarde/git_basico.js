31d8206196002bec40d764a8aae766e8315b246c
MBTGrazielle <grazimelo72@gmail.com>
2022-09-05 18:13:21 -0300

    Estudo do dia 05/09/2022 (tarde)

--- 05-09-2022 -tarde/git_basico.js
+++ 05-09-2022 -tarde/git_basico.js
@@ -1,2 +1,67 @@
 '-------------------------------------------------------------------------------------------'
-aqsqasqfdgfdcgdgd
+//Git: controlador de versões
+//Github: plataforma para armazenação
+'-------------------------------------------------------------------------------------------'
+//Branch:para duplicar projeto alterando partes de forma separadamente
+//git branch: visualiza ramificações existentes
+//Criar e entrar na ramificação:
+//1ª opção:
+//git branch nome: cria nova ramificação
+//git checkout nome: entra na ramificação escolhida
+//2 opção:
+//git checkout -b nome: cria e entra na ramificação com um único comando
+//como excluir ramificação:
+//git branch -d nomedoramo
+'-------------------------------------------------------------------------------------------'
+//git init: inicializar repositório vazio
+'-------------------------------------------------------------------------------------------'
+//git clone: clona/baixa repositório remoto para local(computador)pode ser feito:
+// download do arquivo zip ou clonando no terminal através do HTTPS ou SSH
+'-------------------------------------------------------------------------------------------'
+//Fork: cópia do repositório no espaço remoto(não altera a original, vira nosso)
+'-------------------------------------------------------------------------------------------'
+//git status: verifica as alterações realizadas
+'-------------------------------------------------------------------------------------------'
+//git add: adiciona 1 ou mais arquivos para ser commitado. Pode ser feito:
+//git add . adiciona todos de vez
+//git add nomedoarquivo adiciona algo específico
+'-------------------------------------------------------------------------------------------'
+//git reset: faz exatamete o contrário do git add: Remove 1 ou mais arquivos da área de preparação (quando fica verde) 
+//git reset . remove todos de vez
+//git reset nome remove algo específico
+'-------------------------------------------------------------------------------------------'
+//git commit: cria um marco no projeto com mensagem descritiva para identificar registros do código
+'-------------------------------------------------------------------------------------------'
+//git remote add origin link : vincula repositório local(computador) a um repositório remoto
+'-------------------------------------------------------------------------------------------'
+//git push origin nomedoramo: empurra para espaço remoto
+'-------------------------------------------------------------------------------------------'
+//git pull: baixa alterações do repositório remoto para local(computador)
+//git pull origin nomedoramo
+'-------------------------------------------------------------------------------------------'
+//git log: mostra os commits feitos
+//git log -p : mostra os commits feitos de forma detalhada
+//git logo --oneline: mostra os commits feitos em uma linha resumida
+'-------------------------------------------------------------------------------------------'
+//para enviar algo:
+//1ª opção: Iniciando do zero
+//git init
+//git add
+//git commit
+//git push link
+//2ª opção:Iniciando do zero
+//git init
+//git add
+//git commit
+//git remote add origin linkdorepositorio
+//git push origin nomedoramo
+//3ª opção: Repositório já existe ( não precisa usar o git init porque já foi inicializado em outro momento)
+//git add
+//git commit
+//git push link
+//4ª opção: Repositório já existe (não precisa usar o git init porque já foi inicializado em outro momento)
+//git add
+//git commit
+//git remote add origin linkdorepositorio
+//git push origin nomedoramo
+'-------------------------------------------------------------------------------------------'
---
+++ 05-09-2022 -tarde/variaveis.js
@@ -0,0 +1,38 @@
+'-------------------------------------------------------------------------------------------'
+//var: não é utilizado mais, let veio para corrigir o bug dele
+'-------------------------------------------------------------------------------------------'
+//const: tipo de variável que não é possível reatribuir valor
+'-------------------------------------------------------------------------------------------'
+//let: tipo de variável que é possível reatribuir valor
+//reatribuir valor:
+//let nome = "Grazi";
+//nome = "Gabriela";
+//Observa que não precisa declarar a variável novamente!
+'-------------------------------------------------------------------------------------------'
+//Guardando texto: tem que ser entre aspas, pode ser simples("") ou duplas ('')
+'-------------------------------------------------------------------------------------------'
+//Guardando número: tem que ser entre aspas, pode ser simples("") ou duplas ('')
+'-------------------------------------------------------------------------------------------'
+//Boas práticas: criar variáveis com primeira letra minúsculas
+'-------------------------------------------------------------------------------------------'
+//alguns tipos de variáveis:
+//string: texto
+//number: número
+//boolean: verdadeiro/falso
+'-------------------------------------------------------------------------------------------'
+//como imprimir no console puxando valores de variáveis
+// let nome = "Alane";
+// let idade = 25;
+//imprimir : Alane tem 25 anos!
+// console.log(`${nome} tem  ${idade} anos.`)//StringTemplate
+// console.log(nome, "tem ", idade, "anos");//Concatenação
+// console.log(nome + "tem " + idade + "anos");//Concatenação
+// console.log(nome, 'tem ', idade, 'anos');//Concatenação
+// console.log(nome + 'tem ' + idade + 'anos');//Concatenação
+'-------------------------------------------------------------------------------------------'
+//atribuição de valor da variável:
+// palavra      etiqueta     conteúdo
+// reservada    da caixa     valor
+// let          nome1 =      "Grazi";
+// const        nome2 =      "Grazi";
+'-------------------------------------------------------------------------------------------'
