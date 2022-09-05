f68093278c8e70a13f607eb67bb304357bb37989
MBTGrazielle <grazimelo72@gmail.com>
2022-09-05 18:31:48 -0300

    Link final do dia 05/09/2022

---
+++ 05-09-2022 -tarde/exercicio2_variaveis.js
@@ -0,0 +1,11 @@
+//1.criar variável mutável
+//2.guardar um número qualquer nela
+//3.imprimir no terminal
+//4.reatribuir valor, inserindo novo número
+//3.imprimir no terminal
+
+let numero = 2;
+console.log(numero);
+
+numero = 5;
+console.log(numero);
---
+++ 05-09-2022 -tarde/exercio1_variaveis.js
@@ -0,0 +1,5 @@
+//1.criar variável imutável
+//2.guardar o nome da cidade natal
+//3.imprimir no terminal
+const cidadeNatal = 'Pojuca';
+console.log(cidadeNatal);
